70338c41a46efb6f25638ade8600135124bae8bf
muhhizbe <abumukhtar88@gmail.com>
2020-07-04 11:10:48 +0700

    add component navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,26 @@
+<div>
+    <!-- An unexamined life is not worth living. - Socrates -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+    <div class="container">    
+        <a class="navbar-brand" href="#">Sanber forum</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/pertanyaan">Questions</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+    </nav>
+</div>
